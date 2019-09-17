1460d4e3c724e09e793261f69312627979e9dcbb
William Malkin <rc0039066@student.riversidecollege.ac.uk>
2019-09-17 11:41:01 +0100

    numbers1

--- Numbers/Form1.Designer.cs
+++ Numbers/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(308, 115);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(459, 157);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
