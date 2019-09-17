d09bce24df3a64d8c78dacde91dcb2273fcf5a2d
William Malkin <rc0039066@student.riversidecollege.ac.uk>
2019-09-17 12:46:17 +0100

    Numbers V3

--- Numbers/Form1.cs
+++ Numbers/Form1.cs
@@ -27,13 +27,38 @@ namespace Numbers
             richTextBox1.AppendText(c.ToString("0.000000E0") + "\n");
             richTextBox1.AppendText(c.ToString("0.000E0") + "\n");
 
-
-
-
+            string st = "three";
+            try
+            {
+                int it = int.Parse(st);
+            }
+            catch
+            {
+                MessageBox.Show(st + " is not an intiger");
+            }
+            string s2 = "3.2";
+            try
+            {
+                int i2 = int.Parse(s2);
+            }
+            catch
+            {
+                MessageBox.Show(s2 + "is not an interger");
+                int i2 = 0;
+            }
+            string s3 = "pi";
+            try
+            {
+                double d2 = double.Parse(s3);
+            }
+            catch
+            {
+                double d2 = 0;
+            }
         }
-
         private void RichTextBox1_TextChanged(object sender, EventArgs e)
         { 
+
     }   }
 
 
