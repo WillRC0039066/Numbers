aaaff41b3b3d9889b32fb2aa56d20f7f02584efd
William Malkin <rc0039066@student.riversidecollege.ac.uk>
2019-09-17 12:21:33 +0100

    Numbers V2

--- Numbers/Form1.cs
+++ Numbers/Form1.cs
@@ -15,18 +15,24 @@ namespace Numbers
         public Form1()
         {
             InitializeComponent();
-            double d1 = 5e6;
-            richTextBox1.AppendText(d1.ToString() + "\n");
-            richTextBox1.AppendText(d1.ToString("N") + "\n");
-            richTextBox1.AppendText(d1.ToString("N3") + "\n");
-            richTextBox1.AppendText(d1.ToString("N0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+            double pi = 3.14159265;
+            double c = 299792458;
+            richTextBox1.AppendText(pi.ToString() + "\n");
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+            richTextBox1.AppendText("\n");
+            richTextBox1.AppendText(c.ToString("0") + "\n");
+            richTextBox1.AppendText(c.ToString("N0") + "\n");
+            richTextBox1.AppendText(c.ToString("0.000000E0") + "\n");
+            richTextBox1.AppendText(c.ToString("0.000E0") + "\n");
+
+
+
+
         }
-        
-          private void RichTextBox1_TextChanged(object sender, EventArgs e)
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
         { 
     }   }
 
